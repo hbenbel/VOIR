4aba96faece98bd308850ec2044b8317ec80541c
Hussem Ben Belgacem <hussem.benbelgacem@gmail.com>
2019-02-08 00:56:57 +0100

    [DETECTOR] Adding completed image classifier for square shaped objects

--- src/detector/main.py
+++ src/detector/main.py
@@ -6,19 +6,24 @@ import os
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--image", type=str, help="path to the image to be analyzed", required=True)
-    parser.add_argument("-t", "--threshold", type=int, help="threshold for canny", required=False)
+    parser.add_argument("-ct", "--canny_threshold", type=int, help="threshold for canny", required=False)
+    parser.add_argument("-mcas", "--min_contour_area_size", type=int, help="threshold to determine the minimal square size", required=False)
     args = parser.parse_args()
 
+    print("Loading Image ...")
     image = cv2.imread(args.image)
-    sf = SquareFinder(image, args.threshold)
+    print("Image Loaded !")
+    sf = SquareFinder(image, args.canny_threshold, args.min_contour_area_size)
+    print("Searching for square shaped objects ..")
     sf.FindSquares()
+    print("Search completed !")
     sf.RemoveBorders()
     sf.DrawSquares(image)
     contain_squares = sf.BinaryClassification()
     
     folder = './positive' if contain_squares else './negative'
-    os.mkdir(folder)
-    cv2.imwrite(folder + '/' + args.image.split("/")[-1])
+    os.makedirs(folder, exist_ok=True)
+    cv2.imwrite(folder + '/' + args.image.split("/")[-1], image)
     print("The image was put in the folder ", folder)
 
 if __name__ == "__main__":
--- src/detector/square_finder.py
+++ src/detector/square_finder.py
@@ -1,26 +1,51 @@
 import cv2
+from math import sqrt
+import numpy as np
+
+
+def computeCosine(anchor, pt1, pt2, epsilon = 1e-10):
+    x1 = pt1[0] - anchor[0]
+    y1 = pt1[1] - anchor[1]
+    x2 = pt2[0] - anchor[0]
+    y2 = pt2[1] - anchor[1]
+    return (x1 * x2 + y1 * y2) / (sqrt((x1**2 + y1**2) * (x2**2 + y2**2)) + epsilon)
 
 
 class SquareFinder():
-    def __init__(self, image, canny_threshold):
+    def __init__(self, image, canny_threshold, min_contour_area_size):
         self.image = cv2.medianBlur(image, 9)
-        self.gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.canny_threshold = 50 if canny_threshold is None else canny_threshold
+        self.min_contour_area_size = 100 if min_contour_area_size is None else min_contour_area_size
         self.squares = []
 
     def FindSquares(self):
-        return
+        for channel in range(3):
+            image_single_channel = cv2.extractChannel(self.image, channel)
+            edges = cv2.Canny(image_single_channel, 5, self.canny_threshold, apertureSize = 5)
+            edges = cv2.dilate(edges, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
+            contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for i in range(len(contours)):
+                contour = contours[i]
+                arc_length = 0.02 * cv2.arcLength(contour, True)
+                approximative_contour = cv2.approxPolyDP(contour, arc_length, True)
+                max_cosine = 0
+                if len(approximative_contour) == 4 and cv2.isContourConvex(approximative_contour) and cv2.contourArea(approximative_contour) > self.min_contour_area_size:
+                    for j in range(2, 5):
+                        cosine = computeCosine(approximative_contour[j - 1][0], approximative_contour[j % 4][0], approximative_contour[j - 2][0])
+                        max_cosine = max(max_cosine, cosine)
+                    if max_cosine < 0.3:
+                        self.squares.append(approximative_contour)
 
     def RemoveBorders(self):
         squares = []
         for square in self.squares:
-            if square[0] > 3 and square[1] > 3:
+            if square[0][0][0] > 3 and square[0][0][1] > 3:
                 squares.append(square)
         self.squares = squares
 
     def DrawSquares(self, frame):
         for square in self.squares:
-            cv2.polylines(frame, square, True, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.polylines(frame, square, True, (0, 0, 255), 10)
 
     def BinaryClassification(self):
         return not not self.squares
